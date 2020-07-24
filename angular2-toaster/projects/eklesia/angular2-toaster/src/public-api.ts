4114c46537d1b98aaa73e902ffe35d69cfca5fcc
hugoj-goncalves <hugo159@gmail.com>
2020-07-24 12:18:12 -0300

    WIP

---
+++ angular2-toaster/projects/eklesia/angular2-toaster/src/public-api.ts
@@ -0,0 +1,10 @@
+/*
+ * Public API Surface of angular2-toaster
+ */
+
+export * from './lib/toast.component';
+export * from './lib/toaster-container.component';
+export * from './lib/toaster.service';
+export * from './lib/toaster-config';
+
+export * from './lib/angular2-toaster.module';
